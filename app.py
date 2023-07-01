10affe0e8be0d7dbaf6f849fcb9ab379e7ef8972
pujitha <akshayapujithakolli@gmail.com>
2023-07-01 10:19:35 +0530

    summarizer api done

--- app.py
+++ app.py
@@ -1,22 +1,37 @@
-from flask import Flask
+from flask import Flask,render_template,request
+import requests
+import json
+import os
 
-
-
-app=Flask(__name__)
+API_TOKEN=API_TOKEN = os.environ.get('API_TOKEN')
+app=Flask(__name__,template_folder='src')
 
 @app.route('/')
 def home():
-    pipeline=pipeline("summarization")
-    return "hello world"
+    return render_template("index.html")
 
+def get_summary(input_text):
+    API_URL = "https://api-inference.huggingface.co/models/google/pegasus-cnn_dailymail"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+    
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+    
+    input=json.dumps(input_text)
+    output=query(input)
+    return output
 
-
-
-
-
-
-
+@app.route('/summarizer/',methods=['POST'])
+def summarize():
+    input=request.form.get('text')
+    
+    output_summary=get_summary(input)
+    print(output_summary[0]['summary_text'])
+    return render_template("index.html",output=output_summary[0]['summary_text'])
 
 
 if __name__=="__main__":
-    app.run()
+    if 'API_TOKEN' not in os.environ:
+        raise ValueError("API_TOKEN environment variable is not set.")
+    app.run(debug=True)
